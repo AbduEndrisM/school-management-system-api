8ba06c7c0e8f2b2a6458ea272e2405e274bbe895
abduendrism <beazsasm@gmail.com>
2019-07-17 02:00:09 -0500

    all rest responses are exposed - eureka client configured and tested

--- src/main/java/com/mum/studentmis/StudentmisApplication.java
+++ src/main/java/com/mum/studentmis/StudentmisApplication.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-
+//@EnableDiscoveryClient
 public class StudentmisApplication{
 
 //	@Autowired
